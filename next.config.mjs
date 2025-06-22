0687e45984a182909723d74174b9d17257770885
TriPham9001 <tri.pham1101@gmail.com>
2025-06-22 22:29:04 +0700

    feat(remove package not use): remove package

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,34 @@
 /* eslint-disable import/no-extraneous-dependencies, import/extensions */
 import './src/libs/Env.mjs';
 import withBundleAnalyzer from '@next/bundle-analyzer';
-import createMDX from '@next/mdx';
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const withMDX = createMDX({
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
+/** @type {import('next').NextConfig} */
+export default bundleAnalyzer({
+  output: 'standalone',
+  eslint: {
+    dirs: ['.'],
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack: (config) => {
+    // config.externals is needed to resolve the following errors:
+    // Module not found: Can't resolve 'bufferutil'
+    // Module not found: Can't resolve 'utf-8-validate'
+    config.externals.push({
+      bufferutil: 'bufferutil',
+      'utf-8-validate': 'utf-8-validate',
+    });
+
+    /* eslint-disable no-param-reassign */
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    /* eslint-enable no-param-reassign */
+
+    return config;
   },
 });
-
-/** @type {import('next').NextConfig} */
-export default withMDX(
-  bundleAnalyzer({
-    output: 'standalone',
-    eslint: {
-      dirs: ['.'],
-    },
-    poweredByHeader: false,
-    reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    webpack: (config) => {
-      // config.externals is needed to resolve the following errors:
-      // Module not found: Can't resolve 'bufferutil'
-      // Module not found: Can't resolve 'utf-8-validate'
-      config.externals.push({
-        bufferutil: 'bufferutil',
-        'utf-8-validate': 'utf-8-validate',
-      });
-
-      /* eslint-disable no-param-reassign */
-      config.resolve.alias.canvas = false;
-      config.resolve.alias.encoding = false;
-      /* eslint-enable no-param-reassign */
-
-      return config;
-    },
-  })
-);
